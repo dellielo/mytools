a9a22fc13c81262a2a5b76d68c3bcd003b9ba6dc
dellielo <dellielo03@gmail.com>
2019-09-23 15:26:11 +0200

    add function with endwiths

--- mytools/path.py
+++ mytools/path.py
@@ -6,13 +6,21 @@ def mkdir_and_remove(output_dir):
         shutil.rmtree(output_dir)
     os.makedirs(output_dir, exist_ok=False)
 
-def get_files_in_folder_according_ext(path, fileTypes):
+def get_files_in_folder_according_ext(path, ext_to_compare):
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)):
             basename = os.path.basename(file)
             _, ext = os.path.splitext(basename)
             # ext = FileHelper.getFileExtenstion(file)
-            if(ext.lower() in fileTypes):
+            if(ext.lower() in ext_to_compare):
+                yield os.path.join(path, file), file,  ext
+
+def get_files_in_folder_endswith(path, end_to_compare):
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            basename = os.path.basename(file)
+            _, ext = os.path.splitext(basename)
+            if file.endswith(end_to_compare):
                 yield os.path.join(path, file), file,  ext
 
 def get_just_filename(pathname):
